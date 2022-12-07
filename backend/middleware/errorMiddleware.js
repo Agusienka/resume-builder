424a58d5aea319344acf05c3b2c4d3f76eea39db
agnieszkamogelnicki <agamoe@gmail.com>
2022-12-06 22:06:28 -0600

    auth with protection

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -5,7 +5,13 @@ const errorHandler = (err, req, res, next) => {
 
     res.json({
         message: err.message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack
+        _stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+        get stack() {
+            return this._stack
+        },
+        set stack(value) {
+            this._stack = value
+        },
 
     })
 }
---
+++ backend/models/userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+
+const userSchema = mongoose.Schema(
+    {
+    firstName: {
+        type: String,
+        required: [true, "Please add first name"]
+    },
+    lastName: {
+        type: String,
+        required: [true, "Please add last name"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please add an email"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Please add a password"]
+    },
+    phone: {
+        type: String,
+        required: [true, "Please add phone number"]
+    }
+},
+{
+    timestamps: true
+})
+
+
+module.exports = mongoose.model('User', userSchema)
