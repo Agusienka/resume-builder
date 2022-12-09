c96e795f5e5a042d7d13e132d0a4fb7e44a6eab3
agnieszkamogelnicki <agamoe@gmail.com>
2022-12-09 10:03:22 -0600

    education action slice, reducer, service done

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice'
 import goalReducer from '../features/goals/goalSlice'
 import personalReducer from '../features/personals/personalSlice'
+import educationReducer from '../features/educations/educationSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     goals: goalReducer,
     personals: personalReducer,
+    educations: educationReducer,
   },
 });
