914d1fdb9f3c1b483f124bd0b51080bc6b5d9ac0
Titty Tooty <grant.schlimgen@gmail.com>
2022-12-12 20:42:41 -0600

    b+w template

--- frontend/src/components/ResumeForms.jsx
+++ frontend/src/components/ResumeForms.jsx
@@ -4,26 +4,62 @@ import ResumeData from "./ResumeData";
 export default function ResumeForms() {
   return (
     <>
+      <div class='rHeader'>
       <h1 class='rName'>User McUser</h1>
-      <p class='rEmail'>user@user.com</p>
-      <p class='rPhone'>123-456-7890</p>
+        <div class='rContactInfo'>
+        <p>user@user.com</p>
+        <p>123-456-7890</p>
+        </div>
+        </div>
+      <br/>
+      <br/>
+      <br/>
+      <div class='rBody'>
       <hr></hr>
+      <br/>
+      <br/>
       <h3>Professional Experience</h3>
-      <p>Company Inc.</p>
-      <p>(March 2022-December 2022)</p>
-      <ul>Learned how to be a good worker</ul>
-      <ul>Learned how to listen to boss</ul>
+        <p class ='rtab'>Company Inc. (March 2022-December 2022)</p>
+          <ul class ='rbullet'>Learned how to be a good worker</ul>
+          <ul class ='rbullet'>Learned how to listen to boss</ul>
+      <br/>
+        <p class ='rtab'>Business Store (January 2021-January 2022)</p>
+          <ul class ='rbullet'>Learned how to deal with people</ul>
+          <ul class ='rbullet'>Ran a cash register</ul>
+      <br/>
+      <br/>
       <h3>Education</h3>
-      <p>College University</p>
-      <p>(GPA 3.5, Graduated June 2016)</p>
-      <p>Valley High School</p>
-      <p>(GPA 3.0, Graduated May 2020)</p>
+        <p class='rtab'>College University (GPA 3.5, Graduated June 2016)</p>
+          <p class='rbullet'>Received a B.S. in Coding</p>
+        <p class='rtab'>Valley High School (GPA 3.0, Graduated May 2020)</p>
+      <br/>
+      <br/>
       <h3>Proficiencies</h3>
-      <ul>Proficient in Excel</ul>
-      <ul>Proficient in VSCode</ul>
-      <ul>Proficient in People Skills</ul>
+        <ul class ='rtab'>Proficient in Excel</ul>
+        <ul class ='rtab'>Proficient in VSCode</ul>
+        <ul class ='rtab'>Proficient in People Skills</ul>
+      <br/>
+      <br/>
+      </div>
       <hr></hr>
       <h3>References</h3>  
+      <div>
+      <blockquote class='rRef1'>
+          "Guy Workman <br/>
+          President of Company Inc. <br/>
+          123-321-1234"
+        </blockquote>
+        <blockquote class='rRef2'>
+        "John Smith <br/>
+        Good Dude <br/>
+        123-321-1235"
+        </blockquote>
+        <blockquote class='rRef3'>
+          "Mary Martin <br/>
+          College University Dean <br/>
+          999-999-9999"
+        </blockquote>
+      </div>
     </>
   );
 }
